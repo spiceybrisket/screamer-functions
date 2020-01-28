a463bdf4e1bd88195dd730c49943df3abf4e0523
Adam Spice <adam@namsys.com.au>
2020-01-29 09:17:25 +1300

    initial commit

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2020-01-28T18:54:16.773Z',
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
